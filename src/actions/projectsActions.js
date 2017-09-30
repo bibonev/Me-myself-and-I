9be843f742c372290579d8494c059bfe49a838a4
Boyan Bonev <boyan.i.bonev@gmail.com>
2017-09-30 14:36:04 +0100

    Added pagination for projects.

--- src/actions/projectsActions.js
+++ src/actions/projectsActions.js
@@ -5,9 +5,41 @@ export function showProjects() {
         type: types.SHOW_PROJECTS,
         projects: [
             {
-                name: "Spinal Cord Hackathon"
+                name: "Spinal Cord Hackathon 1",
+                page: 1
             }, {
-                name: "Facebook Hackathon"
+                name: "Facebook Hackathon 2",
+                page: 1
+            }, {
+                name: "Spinal Cord Hackathon 3",
+                page: 1
+            }, {
+                name: "Facebook Hackathon 4",
+                page: 2
+            }, {
+                name: "Spinal Cord Hackathon 5",
+                page: 2
+            }, {
+                name: "Facebook Hackathon 6",
+                page: 2
+            }, {
+                name: "Spinal Cord Hackathon 7",
+                page: 3
+            }, {
+                name: "Facebook Hackathon 8",
+                page: 3
+            }, {
+                name: "Spinal Cord Hackathon 9",
+                page: 3
+            }, {
+                name: "Facebook Hackathon 10",
+                page: 4
+            }, {
+                name: "Spinal Cord Hackathon 11",
+                page: 4
+            }, {
+                name: "Facebook Hackathon 12",
+                page: 4
             }
         ]
     };
--- src/components/projects/ProjectsPage.js
+++ src/components/projects/ProjectsPage.js
@@ -9,9 +9,17 @@ import {Card, CardActions, CardHeader, CardText} from 'material-ui/Card';
 import FlatButton from 'material-ui/FlatButton';
 import ActionCode from 'material-ui/svg-icons/action/code';
 
+import Pagination from 'material-ui-pagination';
+
 class ProjectsPage extends React.Component {
     constructor() {
         super();
+
+        this.state = {
+            totalPages: 4,
+            displayPages: 4,
+            currentPage: 1
+        };
     }
 
     componentWillMount() {
@@ -19,6 +27,10 @@ class ProjectsPage extends React.Component {
             .props
             .actions
             .showProjects();
+    }
+
+    onChangePage(currentPage) {
+        this.setState({currentPage});
     }
 
     render() {
@@ -29,29 +41,46 @@ class ProjectsPage extends React.Component {
                         .props
                         .projects
                         .map(project => {
-                            return (
-                                <ListItem>
-                                    <Card>
-                                        <CardHeader title={project.name} subtitle="Subtitle"/>
-                                        <CardText expandable={false}>
-                                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec mattis pretium
-                                            massa. Aliquam erat volutpat. Nulla facilisi. Donec vulputate interdum
-                                            sollicitudin. Nunc lacinia auctor quam sed pellentesque. Aliquam dui mauris,
-                                            mattis quis lacus id, pellentesque lobortis odio.
-                                        </CardText>
-                                        <CardActions>
-                                            <FlatButton
-                                                href="https://github.com/bibonev/"
-                                                target="_blank"
-                                                label=""
-                                                primary={true}
-                                                icon={< ActionCode />}/>
-                                        </CardActions>
-                                    </Card>
-                                </ListItem>
-                            );
+                            const {currentPage} = this.state;
+                            if (currentPage === project.page) {
+                                return (
+                                    <ListItem>
+                                        <Card>
+                                            <CardHeader title={project.name} subtitle="Subtitle"/>
+                                            <CardText expandable={false}>
+                                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec mattis pretium
+                                                massa. Aliquam erat volutpat. Nulla facilisi. Donec vulputate interdum
+                                                sollicitudin. Nunc lacinia auctor quam sed pellentesque. Aliquam dui mauris,
+                                                mattis quis lacus id, pellentesque lobortis odio.
+                                            </CardText>
+                                            <CardActions>
+                                                <FlatButton
+                                                    href="https://github.com/bibonev/"
+                                                    target="_blank"
+                                                    label=""
+                                                    primary={true}
+                                                    icon={< ActionCode />}/>
+                                            </CardActions>
+                                        </Card>
+                                    </ListItem>
+                                );
+                            }
                         })}
                 </List>
+                <div style={{
+                    textAlign: "center"
+                }}>
+                    <Pagination
+                        styleRoot={{
+                        margin: '2px',
+                        display: "inline-block"
+                    }}
+                        total={this.state.totalPages}
+                        current={this.state.currentPage}
+                        display={this.state.displayPages}
+                        onChange=
+                        { currentPage => this.onChangePage(currentPage) }/>
+                </div>
             </div>
         );
     }

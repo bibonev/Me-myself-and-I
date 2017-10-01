afb6aa6a820ea3cfe7a6aef4104ddf67d8bfd8f7
Boyan Bonev <boyan.i.bonev@gmail.com>
2017-10-01 15:13:39 +0100

    Added chips to the project page and configured the github icon.

--- src/actions/projectsActions.js
+++ src/actions/projectsActions.js
@@ -6,40 +6,76 @@ export function showProjects() {
         projects: [
             {
                 name: "Spinal Cord Hackathon 1",
+                technologies: [
+                    'React', 'Node', 'Express', 'MongoDB'
+                ],
                 page: 1
             }, {
                 name: "Facebook Hackathon 2",
+                technologies: [
+                    'React', 'Node', 'Express', 'MongoDB', 'Node-Red'
+                ],
                 page: 1
             }, {
                 name: "Spinal Cord Hackathon 3",
-                page: 1
+                technologies: [
+                    'C#', '.NET', 'Arduino', 'XNA'
+                ],
+                page: 2
             }, {
                 name: "Facebook Hackathon 4",
+                technologies: [
+                    'React', 'Node', 'Express', 'MongoDB'
+                ],
                 page: 2
             }, {
                 name: "Spinal Cord Hackathon 5",
-                page: 2
+                technologies: [
+                    'React', 'Node', 'Express', 'MongoDB'
+                ],
+                page: 3
             }, {
                 name: "Facebook Hackathon 6",
-                page: 2
+                technologies: [
+                    'React', 'Node', 'Express', 'MongoDB'
+                ],
+                page: 3
             }, {
                 name: "Spinal Cord Hackathon 7",
-                page: 3
+                technologies: [
+                    'React', 'Node', 'Express', 'MongoDB'
+                ],
+                page: 4
             }, {
                 name: "Facebook Hackathon 8",
-                page: 3
+                technologies: [
+                    'React', 'Node', 'Express', 'MongoDB'
+                ],
+                page: 4
             }, {
                 name: "Spinal Cord Hackathon 9",
-                page: 3
+                technologies: [
+                    'React', 'Node', 'Express', 'MongoDB'
+                ],
+                page: 5
             }, {
                 name: "Facebook Hackathon 10",
-                page: 4
+                technologies: [
+                    'React', 'Node', 'Express', 'MongoDB'
+                ],
+                page: 5
             }, {
                 name: "Spinal Cord Hackathon 11",
-                page: 4
+                technologies: [
+                    'React', 'Node', 'Express', 'MongoDB'
+                ],
+                page: 6
             }, {
                 name: "Facebook Hackathon 12",
-                page: 4
+                technologies: [
+                    'React', 'Node', 'Express', 'MongoDB'
+                ],
+                page: 6
             }
         ]
     };
